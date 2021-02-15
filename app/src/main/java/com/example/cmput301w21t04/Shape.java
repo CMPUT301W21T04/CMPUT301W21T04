4a317be8fcdbf8fdb6d4b43843398a03560a5a34
myusername <justineb@ualberta.ca>
2021-02-15 12:43:40 -0700

    Updated color on shape class

--- app/src/main/java/com/example/cmput301w21t04/Shape.java
+++ app/src/main/java/com/example/cmput301w21t04/Shape.java
@@ -1,10 +1,10 @@
 package com.example.cmput301w21t04;
 
 abstract public class Shape {
-    String color = "rainbow";
+    String color = "yellow";
     
     private int x;
     private int y;
-    String color = "yellow";
+
 
 }
